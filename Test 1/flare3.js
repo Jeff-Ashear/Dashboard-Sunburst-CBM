950839105f2285bcfb3b4f41b3f1a7b4970bb876
Jeff.A <kinesthesiac@gmail.com>
2020-07-15 11:09:04 -0700

    Manipulating data to achieve desired display.

---
+++ Test 1/flare3.js
@@ -0,0 +1,170 @@
+const data = {
+    "name": "flare",
+    "children": [
+        {
+            "name": "Website",
+            "children": [
+                {"name": "Completed", "value": 15918},
+                    {
+                    "children": [
+                    {"name": "DB Structure", "value": 3938},
+                    {"name": "Backend Architecture", "value": 3812},
+                    {"name": "Front End Destinations", "value": 2518}                 
+                    ]
+                },
+                {"name": "In Progress", "value": 7210},
+                // {
+                //     "children": [
+                //     {"name": "Issues", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+                {"name": "Coming Soon", "value": 2041},
+                // {
+                //     "children": [
+                //     {"name": "Issues", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+            ],
+        },
+
+        // {
+        //     "name": "Branding",
+        //     "children": [
+        //         {"name": "Completed", "value": 4180},
+        //         //     {
+        //         //     "children": [
+        //         //     {"name": "Tasks", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //         // {"name": "In Progress", "value": 7210},
+        //         // {
+        //         //     "children": [
+        //         //     {"name": "Issues", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //         {"name": "Coming Soon", "value": 2011},
+        //         // {
+        //         //     "children": [
+        //         //     {"name": "Issues", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //     ],
+        // },
+
+        {
+            "name": "Graphic Design",
+            "children": [
+                {"name": "Completed", "value": 4180},
+                //     {
+                //     "children": [
+                //     {"name": "Tasks", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+                {"name": "In Progress", "value": 7210},
+                // {
+                //     "children": [
+                //     {"name": "Issues", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+                {"name": "Coming Soon", "value": 2011},
+                // {
+                //     "children": [
+                //     {"name": "Issues", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+            ],
+        },
+
+        {
+            "name": "SEO",
+            "children": [
+                {"name": "Completed", "value": 6180},
+                //     {
+                //     "children": [
+                //     {"name": "Tasks", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+                {"name": "In Progress", "value": 7210},
+                // {
+                //     "children": [
+                //     {"name": "Issues", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+                {"name": "Coming Soon", "value": 2011},
+                // {
+                //     "children": [
+                //     {"name": "Issues", "value": 3938},
+                //     {"name": "Time below cost", "value": 3812}                 
+                //     ]
+                // },
+            ],
+        },
+
+        // {
+        //     "name": "PPC",
+        //     "children": [
+        //         // {"name": "Completed", "value": 4180},
+        //         //     {
+        //         //     "children": [
+        //         //     {"name": "Tasks", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //         {"name": "In Progress", "value": 7210},
+        //         // {
+        //         //     "children": [
+        //         //     {"name": "Issues", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //         {"name": "Coming Soon", "value": 2011},
+        //         // {
+        //         //     "children": [
+        //         //     {"name": "Issues", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //     ],
+        // },
+
+        // {
+        //     "name": "Other",
+        //     "children": [
+        //         {"name": "Completed", "value": 4180},
+        //         //     {
+        //         //     "children": [
+        //         //     {"name": "Tasks", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //         {"name": "In Progress", "value": 7210},
+        //         // {
+        //         //     "children": [
+        //         //     {"name": "Issues", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //         {"name": "Coming Soon", "value": 2011},
+        //         // {
+        //         //     "children": [
+        //         //     {"name": "Issues", "value": 3938},
+        //         //     {"name": "Time below cost", "value": 3812}                 
+        //         //     ]
+        //         // },
+        //     ],
+        // },
+        
+    ]
+}
+
+export default data;
